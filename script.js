1a52d7c7b0d85d4f7e43f84a356e2cee731b5d02
Gagan714 <gaganursmg@gmail.com>
2024-03-15 12:21:57 +0530

    playbar setting

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
-async function getsongs(){
-    let a=await fetch("http://127.0.0.1:3000/songs/")
-    let response=await a.text();
-    let div=document.createElement("div")
-    div.innerHTML=response
-    let as=div.getElementsByTagName("a")
+async function getsongs() {
+    let a = await fetch("http://127.0.0.1:3000/songs/")
+    let response = await a.text();
+    let div = document.createElement("div")
+    div.innerHTML = response
+    let as = div.getElementsByTagName("a")
     let songs = []
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
-        if(element.href.endsWith(".mp3")){
+        if (element.href.endsWith(".mp3")) {
             songs.push(element.href.split("/songs/")[1])
         }
     }
     return songs
 }
 
-async function main(){
+async function main() {
     let songs = await getsongs()
-    console.log(songs) 
+    console.log(songs)
     let songUL = document.querySelector(".songlist").getElementsByTagName("ul")[0]
     for (const song of songs) {
-        songUL.innerHTML= songUL.innerHTML+ `<li> ${song.replaceAll("%20"," ")}</li>`;
+        songUL.innerHTML = songUL.innerHTML + `<li><img src="music.svg" alt="" class="invert">
+        <div class="info">
+            <div> ${song.replaceAll("%20", " ")}</div>
+            <div>Sky</div>
+        </div>
+        <div class="playnow">
+            <span>Play now</span>
+            <img src="play.svg" alt="" class="invert">
+        </div>
+        </li>`
     }
-    var audio = new Audio(songs[0]);
-    //audio.play();
-   audio.addEventListener("loadeddata", () => {
-    console.log(audio.duration,audio.currentSrc,audio.currentTime)
-});
-
 }
 
 main()
